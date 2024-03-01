075a3efd33c381bbbd7b1a2004204403b3d3a200
ceo-py <sfmf@abv.bg>
2024-03-01 10:54:14 +0200

    Initial

--- main.py
+++ main.py
@@ -15,29 +15,16 @@ def nan_to_zero(items):
         if item == "nan":
             items[i] = 0
 
-errors = 0
-for i in range(1196):
 
-    nan_to_zero(main_sklad[i])
-    nan_to_zero(buffer_sklad[i])
-    nan_to_zero(total[i])
-
-    if buffer_sklad[i][7] and str(buffer_sklad[i][0]).strip() != str(main_sklad[i][0]).strip():
+def compere_two_tabs_col_row_single_value(row, compere_colon, tab_one, tab_two):
+    if tab_one[row][7] and str(tab_one[row][compere_colon]).strip() != str(tab_two[row][compere_colon]).strip():
         print(buffer_sklad[i])
         print(f'Row {i + 6}')
         print()
-        errors +=1
 
-    # if any(str(main_sklad[i][x]).strip() != str(buffer_sklad[i][x]).strip() != str(total[i][x]).strip() for x in range(2)):
-    #     print(f'Row {i + 6}')
-    #
-    #     for item in range(2):
-    #         if str(main_sklad[i][item]) == str(buffer_sklad[i][item]) == str(total[i][item]):
-    #             continue
-    #         print(f'{str(total[i][item])} - Сборно')
-    #         print(f'{str(main_sklad[i][item])} - Основен Склад')
-    #         print(f'{str(buffer_sklad[i][item])} - Буферен Склад')
 
-        print()
+for i in range(1196):
+    nan_to_zero(buffer_sklad[i])
+    nan_to_zero(main_sklad[i])
+    compere_two_tabs_col_row_single_value(i, 0, buffer_sklad, main_sklad)
 
-print(errors)
